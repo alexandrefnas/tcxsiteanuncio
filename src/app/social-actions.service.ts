d70804d127b34842327eb11c2d8599b2cf8a80e6
alexandrefnas <alexandrefnas@gmail.com>
2025-10-07 14:42:08 -0300

    Refatorando codigo.

--- src/app/social-actions.service.ts
+++ src/app/social-actions.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Input } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SocialActionsService {
+
+  @Input() title: string = '';
   constructor() {}
 
   executarAcao(rede: string) {
